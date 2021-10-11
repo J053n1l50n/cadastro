7b677221c7fc5dc14330217b4ae9b257c61f8c6b
J053n1l50n <84155579+J053n1l50n@users.noreply.github.com>
2021-10-11 00:36:37 -0300

    Add files via upload

---
+++ cadastro.java
@@ -0,0 +1,84 @@
+package com.cadastrao.cadastramento;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class cadastro {
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false)
+	private long cpf;
+	@Column
+	private String esc1;
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		cadastro other = (cadastro) obj;
+		return id == other.id;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public long getCpf() {
+		return cpf;
+	}
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+	
+
+	public String getEsc1() {
+		return esc1;
+	}
+	public void setEscolha1(String esc1) {
+		this.esc1 = esc1;
+	}
+
+
+	public String getEscolha2() {
+		return escolha2;
+	}
+	public void setEscolha2(String escolha2) {
+		this.escolha2 = escolha2;
+	}
+	public String getEscolha3() {
+		return escolha3;
+	}
+	public void setEscolha3(String escolha3) {
+		this.escolha3 = escolha3;
+	}
+	private String escolha2;
+	private String escolha3;
+	@Override
+	public String toString() {
+		return "cadastro [nome=" + nome + ", escolha1=" + esc1 + ", escolha2=" + escolha2 + ", escolha3=" + escolha3
+				+ "]";
+	}
+	
+}
